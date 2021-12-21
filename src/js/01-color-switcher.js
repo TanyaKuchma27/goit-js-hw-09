b2ffe42e162b67859ac883f0372cb98517fcb00d
Tanya Kuchma <kuchma.tanya@ukr.net>
2021-12-21 22:34:25 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener("click", onStartBtnClick);
+stopBtn.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick() {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+    startBtn.setAttribute("disabled", "disabled");
+    stopBtn.removeAttribute("disabled");
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+}
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", "disabled");
+}
